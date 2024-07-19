7b488aca0abe721a0e682532bf02840657da41c0
DmytroHoliaka <holidim39@gmail.com>
2024-07-19 08:40:14 +0300

    Server: Change CORS policy

--- TaskTracker.Server/Program.cs
+++ TaskTracker.Server/Program.cs
@@ -1,62 +1,59 @@
 using Microsoft.EntityFrameworkCore;
 using TaskTracker.Server.Data;
 
-namespace TaskTracker.Server
+namespace TaskTracker.Server;
+
+public abstract class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddDbContext<TaskTrackerContext>(
+            options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("TaskTrackerContext") ??
+                    throw new InvalidOperationException("Connection string 'TaskTrackerContext' not found.")));
+
+        // Add services to the container.
+        builder.Services.AddCors(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddDbContext<TaskTrackerContext>(
-                options =>
-                    options.UseSqlServer(
-                        builder.Configuration.GetConnectionString("TaskTrackerContext") ??
-                        throw new InvalidOperationException("Connection string 'TaskTrackerContext' not found.")));
-
-            // Add services to the container.
-            builder.Services.AddCors(
-                options =>
+            options.AddDefaultPolicy(
+                policy =>
                 {
-                    options.AddPolicy(
-                        "AllowAll",
-                        builder =>
-                        {
-                            builder.AllowAnyOrigin()
-                                .AllowAnyMethod()
-                                .AllowAnyHeader();
-                        });
+                    policy.WithOrigins("https://localhost:5173");
+                    policy.AllowAnyHeader();
+                    policy.AllowAnyMethod();
                 });
+        });
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+        var app = builder.Build();
 
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseCors("AllowAll");
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.MapFallbackToFile("/index.html");
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseCors();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.MapFallbackToFile("/index.html");
+
+        app.Run();
     }
 }
