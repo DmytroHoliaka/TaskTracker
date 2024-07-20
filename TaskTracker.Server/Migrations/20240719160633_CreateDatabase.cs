1cefd91db0b2f883a66b514bcba74abcc8774cb2
DmytroHoliaka <holidim39@gmail.com>
2024-07-20 10:31:45 +0300

    Server: Change database struction

--- TaskTracker.Server/Migrations/20240719160633_CreateDatabase.cs
+++ TaskTracker.Server/Migrations/20240719160633_CreateDatabase.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -14,10 +15,9 @@ namespace TaskTracker.Server.Migrations
                 name: "ToDoItems",
                 columns: table => new
                 {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
                     Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                    State = table.Column<int>(type: "int", nullable: false)
                 },
                 constraints: table =>
                 {
