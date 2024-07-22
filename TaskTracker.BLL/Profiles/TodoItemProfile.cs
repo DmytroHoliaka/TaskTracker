ba3294ddfaeeeebac3d570a20770b32d0741a8f2
DmytroHoliaka <holidim39@gmail.com>
2024-07-22 10:01:00 +0300

    Server: Refactor AutoMapper profile

--- TaskTracker.BLL/Profiles/TodoItemProfile.cs
+++ TaskTracker.BLL/Profiles/TodoItemProfile.cs
@@ -9,15 +9,8 @@ public class TodoItemProfile : Profile
     public TodoItemProfile()
     {
         CreateMap<TodoItem, TodoItemDto>()
-            .ForMember(
-                dto => dto.Title,
-                config => config.MapFrom(model => $"Model to dto!!!: {model.Title}"))
-            .ConstructUsing(model => new TodoItemDto(model.Id, model.Title, model.State));
-
-        CreateMap<TodoItemDto, TodoItem>()
-            .ForMember(
-                model => model.Title,
-                config => config.MapFrom(dto => $"Dto to model!!!: {dto.Title}"))
+            .ConstructUsing(model => new TodoItemDto(model.Id, model.Title, model.State))
+            .ReverseMap()
             .ConstructUsing(dto => new TodoItem(dto.Id, dto.Title, dto.State));
     }
 }
